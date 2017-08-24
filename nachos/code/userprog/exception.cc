ebe665bc2963c66212f5576c90c0c17cb4e66cb8
Saket <saket.sj55@gmail.com>
2017-08-24 05:22:18 +0000

    Segfault in fork

--- nachos/code/userprog/exception.cc
+++ nachos/code/userprog/exception.cc
@@ -1,4 +1,4 @@
-// exception.cc 
+// exception.cc
 //  Entry point into the Nachos kernel from user programs.
 //  There are two kinds of things that can cause control to
 //  transfer back to here from user code:
@@ -9,7 +9,7 @@
 //
 //  exceptions -- The user code does something that the CPU can't handle.
 //  For instance, accessing memory that doesn't exist, arithmetic errors,
-//  etc.  
+//  etc.
 //
 //  Interrupts (which can also cause control to transfer from user
 //  code into the Nachos kernel) are handled elsewhere.
@@ -18,7 +18,7 @@
 // Everything else core dumps.
 //
 // Copyright (c) 1992-1993 The Regents of the University of California.
-// All rights reserved.  See copyright.h for copyright notice and limitation 
+// All rights reserved.  See copyright.h for copyright notice and limitation
 // of liability and disclaimer of warranty provisions.
 
 #include "copyright.h"
@@ -41,12 +41,12 @@
 //      arg3 -- r6
 //      arg4 -- r7
 //
-//  The result of the system call, if any, must be put back into r2. 
+//  The result of the system call, if any, must be put back into r2.
 //
 // And don't forget to increment the pc before returning. (Or else you'll
 // loop making the same system call forever!
 //
-//  "which" is the kind of exception.  The list of possible exceptions 
+//  "which" is the kind of exception.  The list of possible exceptions
 //  are in machine.h.
 //----------------------------------------------------------------------
 static Semaphore *readAvail;
@@ -54,6 +54,21 @@ static Semaphore *writeDone;
 static void ReadAvail(int arg) { readAvail->V(); }
 static void WriteDone(int arg) { writeDone->V(); }
 
+void func (int arg)
+{
+    if (threadToBeDestroyed != NULL) {
+        delete threadToBeDestroyed;
+    threadToBeDestroyed = NULL;
+    }
+
+#ifdef USER_PROGRAM
+    if (currentThread->space != NULL) {     // if there is an address space
+        currentThread->RestoreUserState();     // to restore, do it.
+    currentThread->space->RestoreContextOnSwitch();
+    }
+#endif
+    machine->Run();
+}
 static void ConvertIntToHex (unsigned v, Console *console)
 {
    unsigned x;
@@ -212,17 +227,30 @@ ExceptionHandler(ExceptionType which)
         machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
         machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
     }else if ((which == SyscallException) && (type == SysCall_Fork)){
+
         NachOSThread *newThread = new NachOSThread("newThread");
-        new_space = new ProcessAddrSpace();
+        ProcessAddressSpace* new_space = new ProcessAddressSpace();
+        new_space->setAllParameters(currentThread->space->numVirtualPages);
         newThread->space = new_space;
-        for(int ptr_for_phy_addr = 0; ptr_for_phy_addr < currentThread->space->numVirtualPages; i++){
-            machine->mainMemory[new_space->KernelPageTable[ptr_for_phy_addr]] = machine->mainMemory[currentThread->space->KernelPageTable[ptr_for_phy_addr]];
+        for(int ptr_for_phy_addr = 0; ptr_for_phy_addr < currentThread->space->numVirtualPages; ptr_for_phy_addr++){
+            for (int vaddr = 0; vaddr < PageSize; vaddr++)
+                machine->mainMemory[(new_space->KernelPageTable[ptr_for_phy_addr].physicalPage)*PageSize+vaddr] = machine->mainMemory[(currentThread->space->KernelPageTable[ptr_for_phy_addr].physicalPage)*PageSize+vaddr];
         }
-        
+        machine->WriteRegister(2, 0);
+        newThread->SaveUserState();
+        machine->WriteRegister(2, newThread->pid);
 
+        void (*foo)(int);
+        foo = &func;
 
-
-
+        // Update the PC of child and parent
+        machine->WriteRegister(PrevPCReg, machine->ReadRegister(PCReg));
+        machine->WriteRegister(PCReg, machine->ReadRegister(NextPCReg));
+        machine->WriteRegister(NextPCReg, machine->ReadRegister(NextPCReg)+4);
+        newThread->userRegisters[PrevPCReg] = currentThread->userRegisters[PrevPCReg];
+        newThread->userRegisters[PCReg] = currentThread->userRegisters[PCReg];
+        newThread->userRegisters[NextPCReg] = currentThread->userRegisters[NextPCReg];
+        newThread->ThreadFork(foo, 0);
     }
     else
     {
@@ -230,4 +258,4 @@ ExceptionHandler(ExceptionType which)
         ASSERT(FALSE);
     }
 }
-    
+
